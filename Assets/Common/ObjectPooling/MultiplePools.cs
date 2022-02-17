f595fa88e3838b43004be51c0d45ae8cc047b745
TranVietAn <tranvietan08@gmail.com>
2022-02-17 16:03:20 +0700

    Object pooling: Add ActiveOnSpawn option

--- Assets/Common/ObjectPooling/MultiplePools.cs
+++ Assets/Common/ObjectPooling/MultiplePools.cs
@@ -2,29 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MultiplePools : MonoBehaviour
+public class MultiplePools : MonoSingleton<MultiplePools>
 {
     [SerializeField]
     private Transform _poolPos;
     [SerializeField]
-    private int _perPoolLimit;
+    private int _defaultPoolSize = 10;
 
     // key = object InstanceId
     Dictionary<int, PoolInMultiplePool> _pools = new Dictionary<int, PoolInMultiplePool>();
 
-    public GameObject SpawnGameObject(GameObject go)
+    public GameObject SpawnGameObject(GameObject go, int poolSize = 0, bool activeOnSpawn = true)
     {
         _pools.TryGetValue(go.GetInstanceID(), out var pool);
 
         if (pool == null)
         {
             pool = ScriptableObject.CreateInstance<PoolInMultiplePool>();
-            pool.Init(go, _poolPos.position, _perPoolLimit);
+            pool.Init(go, _poolPos.position, poolSize == 0 ? _defaultPoolSize : poolSize);
 
             _pools[go.GetInstanceID()] = pool;
         }
 
-        return pool.SpawnObject();
+        return pool.SpawnObject(activeOnSpawn);
     }
 }
 
@@ -32,7 +32,7 @@ public class PoolInMultiplePool : ScriptableObject
 {
     private GameObject _rootObject;
     private Vector3 _poolPos;
-    private int _limit;
+    private int _poolSize;
 
     private Stack<GameObject> _available = new Stack<GameObject>();
 
@@ -40,14 +40,16 @@ public class PoolInMultiplePool : ScriptableObject
     {
         _rootObject = go;
         _poolPos = poolPos;
-        _limit = limit;
+        _poolSize = limit;
     }
 
-    public GameObject SpawnObject()
+    public GameObject SpawnObject(bool activeOnSpawn)
     {
         GameObject go;
         if (_available.Count == 0)
         {
+            _rootObject.SetActive(activeOnSpawn);
+
             go = Instantiate(_rootObject);
             var objectInPool = go.AddComponent<ObjectInMultiplePoolsPool>();
             objectInPool.AssignParrentPool(this);
@@ -58,17 +60,17 @@ public class PoolInMultiplePool : ScriptableObject
 
         if (go == null || go.activeSelf)
         {
-            go = SpawnObject();
+            go = SpawnObject(activeOnSpawn);
         }
 
-        go.SetActive(true);
+        go.SetActive(activeOnSpawn);
 
         return go;
     }
 
     public void BackToPool(GameObject go)
     {
-        if (_available.Count + 1 > _limit)
+        if (_available.Count + 1 > _poolSize)
         {
             Destroy(go);
             return;
---
+++ Assets/Common/ObjectPooling/ObjectSpawner.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _objectInPool;
+    [SerializeField]
+    private List<GameObject> _randomObjList;
+    [SerializeField]
+    private Transform _parrentTransform;
+    [SerializeField]
+    private Transform _spawnPos;
+    [SerializeField]
+    private Vector3 _randomRangePos;
+    [SerializeField]
+    private int _poolSize;
+
+    private Stack<GameObject> _available = new Stack<GameObject>();
+
+    public void Spawn()
+    {
+        var go = SpawnAndReturnObject();
+        SetupObjectTransform(go);
+        go.SetActive(true);
+    }
+
+    public void Spawn(Vector3 position)
+    {
+        var go = SpawnAndReturnObject();
+        go.transform.position = position;
+        go.SetActive(true);
+    }
+
+    public void SpawnRandomObject()
+    {
+        SpawnRandomAndReturnObject();
+    }
+
+    public GameObject SpawnRandomAndReturnObject()
+    {
+        if (_randomObjList.Count == 0)
+        {
+            return null;
+        }
+
+        var targetPrefab = _randomObjList[Random.Range(0, _randomObjList.Count)];
+
+        var go = SpawnAndReturnObject();
+        SetupObjectTransform(go);
+
+        return go;
+    }
+
+    public GameObject SpawnAndReturnObject()
+    {
+        return MultiplePools.Instance.SpawnGameObject(_objectInPool, _poolSize, false);
+    }
+
+    public void ReturnToPool(GameObject go)
+    {
+        if (_available.Count >= _poolSize)
+        {
+            Destroy(go);
+            return;
+        }
+
+        go.SetActive(false);
+        _available.Push(go);
+    }
+
+    Vector3 _bufferVector = Vector3.zero;
+    public void SpawnWithRandomRange()
+    {
+        _bufferVector.x = Random.Range(-_randomRangePos.x, _randomRangePos.x);
+        _bufferVector.y = Random.Range(-_randomRangePos.y, _randomRangePos.y);
+        _bufferVector.z = Random.Range(-_randomRangePos.z, _randomRangePos.z);
+
+        var go = SpawnAndReturnObject();
+        if (_parrentTransform != null)
+        {
+            go.transform.parent = _parrentTransform;
+        }
+
+        if (_spawnPos == null)
+        {
+            go.transform.position += _bufferVector;
+            return;
+        }
+
+        _bufferVector += _spawnPos.position;
+
+        go.transform.position = _bufferVector;
+
+        go.SetActive(true);
+
+    }
+
+    private void SetupObjectTransform(GameObject go)
+    {
+        go.transform.parent = _parrentTransform;
+
+        if (_spawnPos == null)
+        {
+            go.transform.position = _objectInPool.transform.position;
+            return;
+        }
+        go.transform.position = _spawnPos.position;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (_spawnPos == null)
+        {
+            return;
+        }
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(_spawnPos.position, _randomRangePos * 2f);
+    }
+}
